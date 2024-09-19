8beb954042e9ee8d950a2346662e2f5de57c3b88
jaswanth20002 <gurujala.j@northeastern.edu>
2024-09-19 11:52:34 -0400

    Assignment_1 files

---
+++ Assignment_1/src/model/Product.java
@@ -0,0 +1,196 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author jaswa
+ */
+public class Product {
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getQualification() {
+        return qualification;
+    }
+
+    public void setQualification(String qualification) {
+        this.qualification = qualification;
+    }
+
+    public String getCurrentjobprofile() {
+        return currentjobprofile;
+    }
+
+    public void setCurrentjobprofile(String currentjobprofile) {
+        this.currentjobprofile = currentjobprofile;
+    }
+
+    public String getDrivinglicensenumber() {
+        return drivinglicensenumber;
+    }
+
+    public void setDrivinglicensenumber(String drivinglicensenumber) {
+        this.drivinglicensenumber = drivinglicensenumber;
+    }
+
+    public String getDrivinglicensestate() {
+        return drivinglicensestate;
+    }
+
+    public void setDrivinglicensestate(String drivinglicensestate) {
+        this.drivinglicensestate = drivinglicensestate;
+    }
+
+    public String getSocialsecuritynumber() {
+        return socialsecuritynumber;
+    }
+
+    public void setSocialsecuritynumber(String socialsecuritynumber) {
+        this.socialsecuritynumber = socialsecuritynumber;
+    }
+
+    public String getAddressline1() {
+        return addressline1;
+    }
+
+    public void setAddressline1(String addressline1) {
+        this.addressline1 = addressline1;
+    }
+
+    public String getAddressline2() {
+        return addressline2;
+    }
+
+    public void setAddressline2(String addressline2) {
+        this.addressline2 = addressline2;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public void setCity(String City) {
+        this.City = City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public void setState(String State) {
+        this.State = State;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getTelephonenumber() {
+        return telephonenumber;
+    }
+
+    public void setTelephonenumber(String telephonenumber) {
+        this.telephonenumber = telephonenumber;
+    }
+
+    public String getFaxnumber() {
+        return faxnumber;
+    }
+
+    public void setFaxnumber(String faxnumber) {
+        this.faxnumber = faxnumber;
+    }
+    String firstname;
+    String lastname;
+    String gender;
+    String age;
+    String nationality;
+    String email;
+    String phone;
+    String qualification;
+    String currentjobprofile;
+    String drivinglicensenumber;
+    String drivinglicensestate;
+    String socialsecuritynumber;
+    String addressline1;
+    String addressline2;
+    String City;
+    String State;
+    String country;
+    String zipcode;
+    String telephonenumber;
+    String faxnumber;
+    
+    
+    
+    
+}
